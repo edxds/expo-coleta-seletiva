9fcb78cd26da8ca12d5f394c01a19a30ed3395de
Eduardo <edux404@gmail.com>
2018-10-04 23:15:18 -0300

    Criado component para mostrar logo

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './logo.css';
+
+const Logo = () => (
+  <img
+    className="logo"
+    src={require('../../assets/logo/unified-logo.svg')}
+    alt="Logotipo do projeto"
+  />
+);
+
+export default Logo;
