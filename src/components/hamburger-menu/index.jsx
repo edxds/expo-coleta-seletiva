ed86e82ff0b40c516bf64df14bdddd5e83d1aa25
Eduardo <edux404@gmail.com>
2018-10-12 22:19:19 -0300

    Icone do drawer atualizado

--- src/components/hamburger-menu/index.jsx
+++ src/components/hamburger-menu/index.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import HamburgerIcon from '../../assets/icons/hamburger-icon.svg';
+import { ReactComponent as DrawerIcon } from '../../assets/icons/drawer-icon.svg';
 
-import './hamburger.scss';
-
-const HamburgerMenu = ({ onClick, showBackground }) => (
-  <button
-    type="button"
-    className={`hamburger-menu ${showBackground ? 'show-bg' : ''}`}
-    onClick={onClick}
-  >
-    <img src={HamburgerIcon} alt="Menu" />
+const HamburgerMenu = ({ onClick }) => (
+  <button type="button" className="drawer-button" onClick={onClick}>
+    <DrawerIcon />
     <div className="bg" />
   </button>
 );
 
 HamburgerMenu.propTypes = {
   onClick: PropTypes.func.isRequired,
-  showBackground: PropTypes.bool,
-};
-
-HamburgerMenu.defaultProps = {
-  showBackground: false,
 };
 
 export default HamburgerMenu;
